1f1dd0e823eadd0f2408ef7bde2796a4eea09538
J. R. Schmid <jrs+git@weitnahbei.de>
2013-01-23 17:08:11 +0100

    Initial commit, just the Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnkidictArdeDear::Application.config.secret_token = '756ed98fb879ecb562fff14296ef02b7947e4b1105991474adf6f467384706ff9b1cc61acd6b77108fc639fa46bc4a1187f80e17aad5b8ea6ac39eb6fc80afea'
