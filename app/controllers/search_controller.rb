a2fe6f9f26a4ad78a8e5c66e139318795dfde40c
J. R. Schmid <jrs+git@weitnahbei.de>
2013-01-24 17:35:46 +0100

    Ignore leading or trailing whitespace in search query.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @notes = Note.where("flds LIKE :q OR STRIP_ARABIC_DIACRITICS(flds) LIKE :q", :q => "%#{params[:q]}%") unless params[:q].blank?
+    @notes = Note.where("flds LIKE :q OR STRIP_ARABIC_DIACRITICS(flds) LIKE :q", :q => "%#{params[:q].strip}%") unless params[:q].blank?
   end
 end
