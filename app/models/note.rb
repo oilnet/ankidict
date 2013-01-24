d053389ac491dd048542d9a912f6e369fd52bde7
J. R. Schmid <jrs+git@weitnahbei.de>
2013-01-24 10:31:11 +0100

    Stop displaying sound links.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,14 @@
 class Note < ActiveRecord::Base
   attr_accessible :flds
 
+  def flds
+    self['flds'].gsub(/\[.*\]/, '') # Remove links to sound files present in some notes.
+  end
+
+  # Anki stores all vocabulary card data (except for the metadata)
+  # in a single database column as a string separated by the Unicode
+  # symbol as seen below.
+
   def source_lang
     flds.split(/\u001F/)[0]
   end
